929ffc0277737343a0b90dcb6f5bee1ca28e1b42
Jake Mitchell <dromazing@gmail.com>
2017-04-24 07:01:52 +0000

    add testing framework

---
+++ tests/admin/FakeTest.js
@@ -0,0 +1,11 @@
+describe('Fake Testing Suite', () => {
+  it('2 + 2', () => {
+    var num = 2 + 2
+    
+    expect(num).toBe(4)
+  })
+  
+  it('truth', () => {
+    expect(true).toBe(true)
+  })
+})
