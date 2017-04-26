7ab71a4c744eb29150b1a4ec18d8b0f32273825e
Jake Mitchell <dromazing@gmail.com>
2017-04-26 01:12:55 +0000

    add plugins naming convention

--- admin/frontend/router/plugins.js
+++ admin/frontend/router/plugins.js
@@ -1,17 +1,25 @@
 'use strict'
 
-import slugify from 'slugify'
-import titleCase from 'title-case'
 import env from '../../../.env'
 
 const plugins = env.plugins
 var routes = []
 
+var toTitle = function (str) {
+  return str.replace('_', ' ').replace(/(\s*)(\w+)/g, (match, space, word) => {
+    return space + word.charAt(0).toUpperCase() + word.substr(1).toLowerCase()
+  })
+}
+
+var toSlug = function (str) {
+  return str.replace('_', '-').toLowerCase()
+}
+
 Object.keys(plugins).forEach((plugin) => {
   if (plugins[plugin].active) {
     routes.push({
-      name: titleCase(plugin),
-      path: slugify(plugin),
+      name: toTitle(plugin),
+      path: toSlug(plugin),
       component: (resolve) => require(['plugins/' + plugin + '/views/index'], resolve)
     })
   }
